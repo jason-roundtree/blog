419522907ff06fc86ff924ef2a5d18b41abe1319
JASON ROUNDTREE <jasontree123@gmail.com>
2020-05-13 17:10:33 -0700

    Cleanup before merge to master

--- web/.next/static/runtime/webpack.js
+++ web/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d22852e63ee5cb953438";
+/******/ 	var hotCurrentHash = "97373e4566036dcc12e2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
